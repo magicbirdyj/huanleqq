b77530a208e063f0c31a6b2aba7ee9aac4a52723
yangjing <12493085@qq.com>
2017-04-09 23:47:33 +0800

    测试签名

--- ThinkPHP/Library/Vendor/wxp/lib/WxPay.Data.php
+++ ThinkPHP/Library/Vendor/wxp/lib/WxPay.Data.php
@@ -147,7 +147,7 @@ class WxPayResults extends WxPayDataBase
 	{
 		//fix异常
 		if(!$this->IsSignSet()){
-                    throw new WxPayException("签名错误！");
+                    throw new WxPayException("签名错误！111");
 		}
 		
 		$sign = $this->MakeSign();
