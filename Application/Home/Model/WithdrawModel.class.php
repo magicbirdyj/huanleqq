31b4925a8c052fd086ce1e33802dce22c6a7f398
yangjing <12493085@qq.com>
2017-04-10 00:27:44 +0800

    我就不信了

--- Application/Home/Model/WithdrawModel.class.php
+++ Application/Home/Model/WithdrawModel.class.php
@@ -4,7 +4,7 @@ use Think\Model;
 
 class  WithdrawModel extends Model {
     protected $fields=array(
-        'withdraw_id','withdraw_no','user_id', 'withdraw_dues', 'payment_no','withdraw_time',
+        'withdraw_id','withdraw_no','user_id', 'withdraw_dues', 'payment_no','withdraw_time','payment_time',
         '_pk'=>'withdraw_id','_autoinc'=>true
     );
 }
